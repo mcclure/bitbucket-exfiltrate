6c789026166812a564bdfe9c8c54e382fd477052
mcc <andi.m.mcclure@gmail.com>
2020-02-01 17:24:23 -0500

    --raw and --sort-access

--- backup.py
+++ backup.py
@@ -8,10 +8,12 @@ from getpass import getpass
 @click.option('--username', '-u', type=click.STRING, required=True, help="Username to log in with")
 @click.option('--email', '-e', type=click.STRING, required=True, help="Email account associated with username to log in with")
 @click.option('--info', '-v', count=True, help="Print info on each repo")
+@click.option('--raw', '-vv', count=True, help="Print full data dump for every repo")
+@click.option('--sort-access', count=True, help="Privates first")
 @click.option('--outdir', default=None, type=click.Path(file_okay=False), help="Directory to backup into")
-def backup(info, outdir, username, email):
-	if not (info or outdir):
-		raise click.ClickException("Please pass EITHER --info, --outdir or --help")
+def backup(info, raw, outdir, username, email, sort_access):
+	if not (info or raw or outdir):
+		raise click.ClickException("Please pass at least one: --outdir, --info, --raw or --help")
 
 	password = getpass()
 	bitbucket = Client(
@@ -21,8 +23,17 @@ def backup(info, outdir, username, email):
         	email))
 	password = None
 
-	repos = Repository.find_repositories_by_owner_and_role(role="owner", client=bitbucket)
+	repos = [x for x in Repository.find_repositories_by_owner_and_role(role="owner", client=bitbucket)]
+	if sort_access:
+		repos.sort(key=lambda x:x.data['is_private'])
+
 	for repo in repos:
-		print(repo)
+		if raw:
+			print(repo.data)
+		if info:
+			print("name:[{}] {} {} {}{}{}{}\n".format(repo.data['name'], repo.data['scm'],
+				"PRIVATE" if repo.data['is_private'] else "public",
+				"\n[" if repo.data['description'] else "", repo.data['description'], "]" if repo.data['description'] else "",
+				"\n(+ wiki)" if repo.data['has_wiki'] else ""))
 
 backup()
