0b1fbbcc78aceec28f0de8804b271829f941c2ba
mcc <andi.m.mcclure@gmail.com>
2020-02-02 11:09:34 -0500

    Make wiki-tweak more simple so it can run

--- andi-specific/wiki-tweak.py
+++ andi-specific/wiki-tweak.py
@@ -4,10 +4,6 @@ import click
 import sys
 import re
 
-from getpass import getpass
-from mercurial import ui,hg,commands
-import mercurial
-
 ops = ("list", "listfull", "collect", "delete")
 
 def foundAny(target, fragments):
@@ -21,19 +17,9 @@ def foundAny(target, fragments):
 @click.argument('operation', type=click.STRING)
 @click.option('--include', type=click.STRING, help="Comma separated require patterns")
 @click.option('--exclude', type=click.STRING, help="Comma separated ban patterns")
-@click.option('--dry-run', count=True, help="Don't do anything that touches disk")
-@click.option('--username', '-u', type=click.STRING, help="Username to log in with (for delete)")
-@click.option('--email', '-e', type=click.STRING, help="Email account associated with username to log in with (for delete)")
-def tweak(operation, dir, include, exclude, dry_run, username, email):
+def tweak(operation, dir, include, exclude):
 	if not operation in ops:
 		raise click.ClickException("Operation must be one of: {}".format( ", ".join(ops)))
-
-	mercurial_ui = None
-	if operation == "delete" and not dry_run:
-		if not (username and email):
-			raise click.ClickException("Operation delete requires --username and --email")
-
-		mercurial_ui = ui.ui()
 
 	seen = None
 
@@ -46,14 +32,7 @@ def tweak(operation, dir, include, exclude, dry_run, username, email):
 		layer1full = os.path.join(dir, layer1)
 		for layer2 in os.listdir(layer1full): # Repo names
 			layer2full = os.path.join(os.path.join(layer1full, layer2), "wiki")
-			repo = None
-			def repoOp(cmd, args, opts):
-				nonlocal repo
-				print("In [{}]\n{} execute:\nhg {} [{}] {{{}}}\n".format(layer2full, "pretend" if dry_run else "will", cmd, ",".join(args), opts))
-				if not dry_run:
-					if not repo:
-						repo = hg.repository(mercurial_ui, layer2full.encode('utf-8'))
-					getattr(commands, cmd)(mercurial_ui, repo, *[x.encode('utf-8') for x in args], **{k.encode('utf-8'):v.encode('utf-8') for (k,v) in opts.items()})
+			deleteFound = None
 			try:
 				for layer3 in os.listdir(layer2full): # Filenames in wiki dir
 					if layer3 == ".hg":
@@ -73,20 +52,26 @@ def tweak(operation, dir, include, exclude, dry_run, username, email):
 						if not seen:
 							seen = {}
 						seen[layer3] = layer3full
-					elif operation == "delete":
+					elif operation == "delete": # The mercurial python interface doesn't work so well so just print out the hg commands to run
 						if not include:
-							raise click.ClickException("Delete operation must set --include, for annoying reasons")
+							raise click.ClickException("Delete operation must set --include")
 						for i in include:
 							if i and layer3.find(i) >= 0:
-								repoOp("remove", ["*" + i + "*"], {})
+								if deleteFound is None:
+									deleteFound = {}
+									print("pushd " + layer2full)
+								deleteFound[i] = True
+								break
+				if deleteFound is not None:
+					for i in deleteFound.keys():
+						print("hg remove " + "*" + i + "*")
 			except FileNotFoundError:
 				pass
 
-			if repo:
-				repoOp("commit", [], {'message':"Automated removal of Bitbucket spam"})
-				repoOp("push", [], {})
-				print("Stop now")
-				sys.exit(0)
+			if deleteFound is not None:
+				print('hg commit -m "Automated removal of Bitbucket spam"')
+				print("hg push")
+				print("popd")
 
 	if seen:
 		for k in sorted(seen.keys()):
